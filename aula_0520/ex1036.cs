e7a31c5824735b16f0efea4c513b784b9bdb7511
MARCILIO TARGINO <105514699+Marciliot@users.noreply.github.com>
2022-05-20 19:26:50 +0000

    ex1036.cs

--- aula_0520/ex1036.cs
+++ aula_0520/ex1036.cs
@@ -7,6 +7,15 @@ class Program{
     double a = double.Parse(v[0]);
     double b = double.Parse(v[1]);
     double c = double.Parse(v[2]);
-    Console.WriteLine()
+    double dela =  b * b - 4 * a * c;
+    double r1 = (-b + Math.Sqrt(dela)) / (2 * a);
+    double r2 = (-b + Math.Sqrt(dela)) / (2 * a);
+
+    if ( dela < 0 || a == 0)
+     Console.WriteLine("Impossivel calcular");
+  else{
+     Console.WriteLine($"R1 ={r1:f5}");
+     Console.WriteLine($"R1 ={r1:f5}");
+    }
   }
 }

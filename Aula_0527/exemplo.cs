9ad8cceb6274ab18ad7e2a59b82786b3359c97a2
MARCILIO TARGINO <105514699+Marciliot@users.noreply.github.com>
2022-05-27 18:42:11 +0000

    exemplo.cs

---
+++ Aula_0527/exemplo.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Program{
+  public static void Main(){
+    Console.WriteLine("Informe o numéro do mês");
+    string mes = Console.ReadLine();
+    switch (mes) {
+      case "1" : Console.Write("janeiro"); break;
+      case "2" : Console.Write("feverio"); break;
+      case "3" : Console.Write("março"); break;
+      case "4" : Console.Write("abril"); break;
+      case "5" : Console.Write("maio"); break;
+      case "6" : Console.Write("junho"); break;
+      case "7" : Console.Write("julho"); break;
+      case "8" : Console.Write("agosto"); break;
+      case "9" : Console.Write("outubro"); break;
+      case "10" : Console.Write("setembro"); break;
+      case "11" : Console.Write("novembro"); break;
+      case "12" : Console.Write("janerio"); break; 
+      default : Console.WriteLine("valor inválido"); break;
+        
+    }
+    
+    
+  }
+}
